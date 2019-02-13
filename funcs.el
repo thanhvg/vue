bbb0a631963dfd0f73ed04bc9d0906aab62b2876
Thanh Vuong <thanhvg@gmail.com>
2019-02-13 00:25:39 -0700

    00

---
+++ funcs.el
@@ -0,0 +1,54 @@
+;;; funcs.el --- react layer funcs file for Spacemacs. -*- lexical-binding: t -*-
+;;
+;; Copyright (c) 2012-2019 Sylvain Benner & Contributors
+;;
+;; Author: Thanh Vuong <thanhvg@gmail.com>
+;; URL: https://github.com/thanhvg
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; LSP
+(defun spacemacs//vue-setup-lsp ()
+  "Setup lsp backend."
+  (if (configuration-layer/layer-used-p 'lsp)
+      (lsp)
+    (message "`lsp' layer is not installed, please add `lsp' layer to your dotfile."))
+  ;; because lsp will override other linters
+  (flycheck-select-checker 'javascript-eslint))
+
+(defun spacemacs//vue-setup-lsp-company ()
+  "Setup lsp auto-completion."
+  (if (configuration-layer/layer-used-p 'lsp)
+      (progn
+        (spacemacs|add-company-backends
+          :backends company-lsp
+          :modes vue-mode
+          :variables company-minimum-prefix-length 2
+          :append-hooks nil
+          :call-hooks t)
+        (company-mode)
+        (fix-lsp-company-prefix))
+    (message "`lsp' layer is not installed, please add `lsp' layer to your dotfile.")))
+
+
+;; Emmet
+(defun spacemacs//vue-turn-on-emmet ()
+  (emmet-mode 1)
+  (setq-local emmet-indent-after-insert nil))
+
+(defun spacemacs//vue-turn-off-emmet ()
+  (emmet-mode -1))
+
+;; Others
+
+(defun spacemacs//vue-setup-yasnippet ()
+  (yas-activate-extra-mode 'js-mode))
+
+(defun spacemacs//vue-setup-eslint ()
+  (flycheck-select-checker 'javascript-eslint))
+
+
+(defun spacemacs//vue-setup-dumb-company ()
+  (spacemacs|add-company-backends :backends company-capf :modes vue-mode))
---
+++ packages.el
@@ -0,0 +1,97 @@
+;;; packages.el --- react layer packages file for Spacemacs. -*- lexical-binding: t -*-
+;;
+;; Copyright (c) 2012-2019 Sylvain Benner & Contributors
+;;
+;; Author: Thanh Vuong <thanhvg@gmail.com>
+;; URL: https://github.com/thanhvg
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+
+(defconst vue-packages
+  '(
+    vue-mode
+    add-node-modules-path
+    company
+    emmet-mode
+    evil-matchit
+    flycheck
+    prettier-js
+    smartparens
+    yasnippet
+    ))
+
+(defun vue/init-vue-mode ()
+  "Initialize my package"
+  (use-package vue-mode
+    :defer t
+    :mode (("\\.vue\\'"  . vue-mode))
+    :config
+    (setq mmm-submode-decoration-level 0)
+    :init
+    (add-hook 'mmm-mode-hook
+              (lambda ()
+                (set-face-background 'mmm-default-submode-face nil)))
+
+    (pcase vue-backend
+      ('lsp (add-hook 'vue-mode-local-vars-hook #'spacemacs//vue-setup-lsp))
+      ('dumb (progn (add-to-list 'spacemacs-jump-handlers-vue-mode 'dumb-jump-go)
+                    (add-to-list 'spacemacs-jump-handlers-vue-html-mode 'dumb-jump-go))))
+
+    ;; (add-hook 'vue-mode-local-vars-hook #'spacemacs//vue-setup-lsp)
+    ;; (add-to-list 'spacemacs-jump-handlers-vue-mode
+    ;;              'dumb-jump-go)
+
+    ;; (add-to-list 'spacemacs-jump-handlers-vue-html-mode
+    ;;              'dumb-jump-go)
+    ))
+
+(defun vue/post-init-add-node-modules-path ()
+  (spacemacs/add-to-hooks #'add-node-modules-path '(css-mode-hook
+                                                    vue-mode-hook
+                                                    vue-html-mode-hook
+                                                    js-mode-hook)))
+
+(defun vue/post-init-company ()
+  (pcase vue-backend
+    ('dumb (spacemacs|add-company-backends :backends company-capf :modes vue-mode))
+    ('lsp (add-hook 'vue-mode-local-vars-hook #'spacemacs//vue-setup-lsp-company))))
+
+(defun vue/post-init-emmet-mode ()
+  ;; (add-hook 'vue-html-mode 'emmet-mode))
+  ;; (spacemacs/add-to-hooks 'emmet-mode '(vue-html-mode-hook)))
+  ;; (spacemacs/add-to-hooks 'emmet-mode '(mmm-vue-html-mode-hook)))
+  (add-hook 'mmm-vue-html-mode-enter-hook #'spacemacs//vue-turn-on-emmet)
+  (add-hook 'mmm-vue-html-mode-exit-hook #'spacemacs//vue-turn-off-emmet))
+
+(defun vue/post-init-evil-matchit ()
+  (with-eval-after-load 'evil-matchit
+    (plist-put evilmi-plugins 'vue-html-mode
+               '((evilmi-simple-get-tag evilmi-simple-jump)
+                 (evilmi-javascript-get-tag evilmi-javascript-jump)
+                 (evilmi-html-get-tag evilmi-html-jump))))
+  (add-hook `vue-mode-hook `turn-on-evil-matchit-mode))
+
+(defun vue/post-init-flycheck ()
+  (with-eval-after-load 'flycheck
+    (dolist (checker '(javascript-eslint))
+      (flycheck-add-mode checker 'vue-mode)
+      (flycheck-add-mode checker 'js-mode)
+      (flycheck-add-mode checker 'vue-html-mode)))
+  (spacemacs/enable-flycheck 'vue-mode))
+
+(defun vue/pre-init-prettier-js ()
+  (dolist (mode '(vue-mode))
+    (add-to-list 'spacemacs--prettier-modes mode)))
+
+(defun vue/post-init-smartparens ()
+  (if dotspacemacs-smartparens-strict-mode
+      (add-hook 'vue-mode-hook #'smartparens-strict-mode)
+    (add-hook 'vue-mode-hook #'smartparens-mode)))
+
+
+(defun vue/post-init-yasnippet ()
+  (spacemacs/add-to-hooks 'spacemacs/force-yasnippet-off '(mmm-vue-html-mode-enter-hook)
+  (spacemacs/add-to-hooks 'spacemacs/load-yasnippet '(mmm-js-mode-enter-hook))))
